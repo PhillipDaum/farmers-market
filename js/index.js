be30767aca43fadf2aa44c273c4165b98603b6fa
Phil <philgdaum@gmail.com>
2024-06-18 14:49:46 -0600

    add responsive navbar

--- js/index.js
+++ js/index.js
@@ -2,3 +2,10 @@
 // function from the codepen for the pop out hamburger menu
 // and will type the classes into my css
 
+const burger = document.querySelector(".burger i");
+const nav = document.querySelector(".nav");
+
+function showHamburger() {
+  burger.classList.toggle("fa-times");
+  nav.classList.toggle("nav-active");
+}
